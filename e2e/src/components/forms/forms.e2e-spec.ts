8d0451dfecbef150f70095aa88f5660f45bb40b5
jjswski <jjswski@gmail.com>
2018-08-26 23:09:30 +0200

    fix e2e test

--- e2e/src/components/forms/forms.e2e-spec.ts
+++ e2e/src/components/forms/forms.e2e-spec.ts
@@ -9,21 +9,16 @@ describe('forms tests', () => {
     browser.get('/');
   });
 
-  it('should display one empty form at starting', () => {
-    const count = page.getFormsCount();
-    expect(count).toEqual(1);
-  });
-
   it('should add root form by add button', () => {
     page.addRootFormByButtonClick();
     const count = page.getFormsCount();
-    expect(count).toEqual(2);
+    expect(count).toEqual(1);
   });
 
   it('should remove root form by delete button', () => {
     page.addRootFormByButtonClick();
     page.deleteFormByButtonClickAt(0);
     const count = page.getFormsCount();
-    expect(count).toEqual(1);
+    expect(count).toEqual(0);
   });
 });
